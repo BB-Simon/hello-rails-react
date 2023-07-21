8c6b327490c20469504d9bb7c011c39e158b808a
BB-Simon <simon.chowdery@gmail.com>
2023-07-21 09:14:29 +0300

    Add an api endpoint to get a random greeting and and api route

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,9 @@
 class GreetingsController < ApplicationController
   def index
   end
-  
+
+  def greeting
+    greeting = Greeting.all.sample
+    render json: greeting
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "greetings#index"
+
+  get '/api/greeting', to: 'greetings#greeting'
 end
